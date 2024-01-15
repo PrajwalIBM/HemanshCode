65edff74b0e169cbcfb2731707164f28ee51d1bb
PrajwalIBM <155616858+PrajwalIBM@users.noreply.github.com>
2024-01-16 00:11:30 +0530

    cart 3.0

--- OrganicStore/Controllers/ProductController.cs
+++ OrganicStore/Controllers/ProductController.cs
@@ -27,75 +27,8 @@ namespace OrganicStore.Controllers
             List<products> res = prod.GetProducts();
             string productsJson = JsonConvert.SerializeObject(res);
             TempData["products"] = productsJson;
-            /*ViewData["products"] = res;*/
-
-            return RedirectToAction("Products","Home",res);            }
-        /*public IActionResult Details()
-        {
-            List<Models.products> list = new List<Models.products>();
            
-
-            try
-            {
-                using (SqlConnection con = new SqlConnection(ConnectionMod.getConnString()))
-                {
-                    con.Open();
-                    using (SqlCommand cmd = con.CreateCommand())
-                    {
-                        cmd.CommandText = "Select * from products";
-                        using(SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            if (reader.HasRows == false)
-                            {
-                                
-                            }
-                            while (reader.Read())
-                            {
-                                products pd = new Models.products();
-                                pd.p_name = reader.GetString("p_name");
-                                pd.details = reader.GetString("details");
-                                pd.category = reader.GetString("category");
-                                *//*pd.s_price = reader.GetDouble(reader.GetOrdinal("s_price"));
-                                pd.o_price = reader.GetFloat(reader.GetOrdinal("o_price"));*//*
-
-                                list.Add(pd);
-                            }
-
-                        }
-                    }
-                    *//*string sql = "SELECT * FROM products";
-                    con.ConnectionString = ConnectionMod.getConnString();
-                    
-                    
-                    DataTable dt = new DataTable();
-                    SqlDataAdapter da = new SqlDataAdapter();
-
-                    da.SelectCommand = new SqlCommand(sql, con);
-                    
-                    
-                    da.Fill(dt);
-
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        var product = new Models.products();
-                        product.p_name = row["p_name"].ToString();
-                        product.details = row["details"].ToString();
-                        product.category = row["category"].ToString();
-                        product.s_price = (float)row["s_price"];
-                        product.o_price = (float)row["o_price"];
-
-                        list.Add(product);
-
-                    }*//*
-                }
-
-                
-                return RedirectToAction("Products","Home",list);
-            }
-            catch (Exception ex)
-            {
-               return View(ex.Message);
-            }*/
+            return RedirectToAction("Products","Home",res);            }
         }
     }
 
